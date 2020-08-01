b27414e898d8186d3b7f1ca93c950bdaefdb0c9a
movesonddd <502348036@qq.com>
2020-08-01 15:13:16 +0800

    环境设置

---
+++ src/env.js
@@ -0,0 +1,22 @@
+let baseURL;
+switch (process.env.NODE_ENV) {
+  case 'development':
+    baseURL = 'http://dev-mall-pre.springboot.cn/api';
+    break;
+  case 'test':
+    baseURL = 'http://test-mall-pre.springboot.cn/api';
+    break;
+  case 'prev':
+    baseURL = 'http://prev-mall-pre.springboot.cn/api';
+    break;
+  case 'prod':
+    baseURL = 'http://mall-pre.springboot.cn/api';
+    break;
+  default:
+    baseURL = 'http://mall-pre.springboot.cn/api';
+    break;
+}
+//这种是jsonp等跨域的配置
+export default {
+  baseURL
+}
